61c4358641d80b02433402b1de7b8d1974297674
xiaohai <hhh18392577760@163.com>
2021-08-07 09:49:59 +0800

    add number

--- src/main/java/com/xiaohai/pojo/User.java
+++ src/main/java/com/xiaohai/pojo/User.java
@@ -3,6 +3,15 @@ package com.xiaohai.pojo;
 public class User {
     private String username;
     private String password;
+    private Integer number;
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
 
     public String getUsername() {
         return username;
